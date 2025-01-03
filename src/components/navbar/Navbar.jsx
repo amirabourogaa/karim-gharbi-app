ac17064790fcc4f948befb9278f40f4922f21955
amirabourogaa <abouragba@u-label.com>
2025-01-03 11:49:24 +0100

    Fix contact page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -31,43 +31,47 @@ const Navbar = () => {
       isScrolled ? 'bg-white  shadow-lg' : 'bg-white'
     }`}
   >
-    <div className="max-w-7xl mx-auto flex items-center justify-between">
-      {/* Logo */}
-      <Link to="/" className="flex items-center">
-        <img src={logo} alt="Logo" className="w-48" />
-      </Link>
+   <div className="max-w-7xl mx-auto flex items-center justify-between">
+  {/* Logo */}
+  <Link to="/" className="flex items-center">
+    <img src={logo} alt="Logo" className="w-48" />
+  </Link>
 
-      {/* Mobile Menu Toggle */}
-      <button
-        onClick={toggleMenu}
-        className="md:hidden text-accent text-3xl focus:outline-none"
-      >
-        {isMenuOpen ? <FaTimes /> : <FaBars />}
-      </button>
+  {/* Mobile Menu Toggle */}
+  <div className="flex items-center space-x-2 md:hidden">
+    <h2 className="text-accent text-lg">Menu</h2>
+    <button
+      onClick={toggleMenu}
+      className="text-accent text-3xl focus:outline-none"
+    >
+      {isMenuOpen ? <FaTimes /> : <FaBars />}
+    </button>
+  </div>
 
-      {/* Menu Links */}
-      <div
-        className={`${
-          isMenuOpen ? 'flex top-16' : 'hidden'
-        } md:flex md:flex-row flex-col items-center md:space-x-8 absolute md:relative left-0 w-full md:w-auto bg-white md:bg-transparent p-4 md:p-0 space-y-4 md:space-y-0`}
-      >
-        <Link to="/" className="text-accent hover:text-primary transition-colors duration-200">
-          Accueil
-        </Link>
-        <Link to="/about" className="text-accent hover:text-primary transition-colors duration-200">
-          Parcours
-        </Link>
-        <Link to="/portfolio" className="text-accent hover:text-primary transition-colors duration-200">
-          Projets
-        </Link>
-        <Link to="/calendar" className="text-accent hover:text-primary transition-colors duration-200">
-          Calendrier
-        </Link>
-        <Link to="/contact" className="text-accent hover:text-primary transition-colors duration-200">
-          Contact
-        </Link>
-      </div>
-    </div>
+  {/* Menu Links */}
+  <div
+    className={`${
+      isMenuOpen ? 'flex top-16' : 'hidden'
+    } md:flex md:flex-row flex-col items-center md:space-x-8 absolute md:relative left-0 w-full md:w-auto bg-white md:bg-transparent p-4 md:p-0 space-y-4 md:space-y-0`}
+  >
+    <Link to="/" className="text-accent hover:text-primary transition-colors duration-200">
+      Accueil
+    </Link>
+    <Link to="/about" className="text-accent hover:text-primary transition-colors duration-200">
+      Parcours
+    </Link>
+    <Link to="/portfolio" className="text-accent hover:text-primary transition-colors duration-200">
+      Projets
+    </Link>
+    <Link to="/calendar" className="text-accent hover:text-primary transition-colors duration-200">
+      Calendrier
+    </Link>
+    <Link to="/contact" className="text-accent hover:text-primary transition-colors duration-200">
+      Contact
+    </Link>
+  </div>
+</div>
+
   </motion.nav>
 
   );
